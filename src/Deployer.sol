f2c206847f5746bdfdd453220c1faba385be7498
rista404 <ristic@risticnikola.com>
2024-10-08 15:10:30 +0200

    add expiry

--- src/Deployer.sol
+++ src/Deployer.sol
@@ -17,15 +17,17 @@ contract Deployer {
         if (bytes(name).length == 0) revert TokenNameEmpty();
         if (bytes(symbol).length == 0) revert TokenSymbolEmpty();
 
+        address treasury = address(0x8d5AF9D897a80afCAf854202ACf6d2641378ba6C);
+
         IHederaTokenService.HederaToken memory token;
         token.name = name;
         token.symbol = symbol;
+        token.treasury = treasury;
 
-        IHederaTokenService.TokenKey[] memory tokenKeys = new IHederaTokenService.TokenKey[](0);
+        IHederaTokenService.Expiry memory expiry = IHederaTokenService.Expiry(0, treasury, 8000000);
+        token.expiry = expiry;
 
-        token.tokenKeys = tokenKeys;
-
-        address createdTokenAddress = HTS.createFungibleToken(token, 0, int32(uint32(decimals)));
+        address createdTokenAddress = HTS.createFungibleToken(token, 100, int32(uint32(decimals)));
 
         tokenAddress = createdTokenAddress;
 
